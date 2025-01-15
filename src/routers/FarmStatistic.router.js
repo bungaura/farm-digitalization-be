77a33749e26b19e6413d7f89d4dd9a9d33804017
Risa <indriklarissa@gmail.com>
2025-01-15 22:28:13 +0700

    feat: add livestock count by farm id endpoint

--- src/routers/FarmStatistic.router.js
+++ src/routers/FarmStatistic.router.js
@@ -3,53 +3,8 @@ const router = Express.Router();
 const farmStatisticController = require("../controllers/FarmStatistic.controller");
 
 router.get(
-  "/get-cows-count/:farmId",
-  farmStatisticController.getCowsCountByFarmId
-);
-
-router.get(
   "/livestock-count/:farmId",
   farmStatisticController.getLivestockCountByFarmId
 );
-
-// router.get(
-//   "/get-female-cows-count-by-farmId/:farmId",
-//   farmStatisticController.getFemaleCowsCountByFarmId
-// );
-
-// router.get(
-//   "/get-male-cows-count-by-farmId/:farmId",
-//   farmStatisticController.getMaleCowsCountByFarmId
-// );
-
-// router.get(
-//   "/get-goats-count-by-farmId/:farmId",
-//   farmStatisticController.getGoatsCountByFarmId
-// );
-
-// router.get(
-//   "/get-female-goats-count-by-farmId/:farmId",
-//   farmStatisticController.getFemaleGoatsCountByFarmId
-// );
-
-// router.get(
-//   "/get-male-goats-count-by-farmId/:farmId",
-//   farmStatisticController.getMaleGoatsCountByFarmId
-// );
-
-// router.get(
-//   "/get-sheep-count-by-farmId/:farmId",
-//   farmStatisticController.getSheepCountByFarmId
-// );
-
-// router.get(
-//   "/get-female-sheep-count-by-farmId/:farmId",
-//   farmStatisticController.getFemaleSheepCountByFarmId
-// );
-
-// router.get(
-//   "/get-male-sheep-count-by-farmId/:farmId",
-//   farmStatisticController.getMaleSheepCountByFarmId
-// );
 
 module.exports = router;
--- src/services/FarmStatistic.service.js
+++ src/services/FarmStatistic.service.js
@@ -1,209 +1,36 @@
 const Lactation = require("../models/Entity/Lactation.model");
 const Livestock = require("../models/Entity/Livestock.model");
+const LivestockType = require("../models/Entity/LivestockType.model");
 const Farm = require("../models/Entity/Farm.model");
 
-exports.getCowsCountByFarmId = async function (params) {
+exports.getLivestockCountByFarmId = async function (path, query) {
   try {
-    const { farmId } = params;
+    const { farmId } = path;
+    const { type, gender, condition, phase, status } = query;
+
     if (!farmId || farmId === ":farmId") {
       throw new Error("Farm ID is required.");
     }
+    const filters = {}
+    if (gender) filters.gender = gender.toUpperCase();
+    if (condition) filters.condition = condition;
+    if (phase) filters.phase = phase.toUpperCase();
+    if (status) filters.status = status;
 
-    const cowsCount = await Livestock.count({
-      where: {
-        farm_id: farmId,
-        type_id: "COW"
-      }
+    includeOptions = []
+    if (type) {
+      includeOptions.push({
+        model: LivestockType,
+        where: { type: type.toUpperCase() }, 
+        attributes: [],
+      });
+    }
+
+    const count = await Livestock.count({
+      where: filters,
+      include: includeOptions
     });
-    return { count: cowsCount };
-
-  } catch (error) {
-    console.error("Error getting number of cows:", error.message);
-    throw error;
-  }
-};
-
-// exports.getFemaleCowsCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const cowsCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "COW",
-//         gender: "FEMALE"
-//       }
-//     });
-//     return { count: cowsCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of female cows:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getMaleCowsCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const cowsCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "COW",
-//         gender: "MALE"
-//       }
-//     });
-//     return { count: cowsCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of male cows:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getGoatsCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const goatsCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "GOAT"
-//       }
-//     });
-//     return { count: goatsCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of goats:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getFemaleGoatsCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const goatsCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "GOAT",
-//         gender: "FEMALE"
-//       }
-//     });
-//     return { count: goatsCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of female goats:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getMaleGoatsCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const goatsCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "GOAT",
-//         gender: "MALE"
-//       }
-//     });
-//     return { count: goatsCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of male goats:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getSheepCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const sheepCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "SHEEP"
-//       }
-//     });
-//     return { count: sheepCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of sheep:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getFemaleSheepCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const sheepCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "SHEEP",
-//         gender: "FEMALE"
-//       }
-//     });
-//     return { count: sheepCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of female sheep:", error.message);
-//     throw error;
-//   }
-// };
-
-// exports.getMaleSheepCountByFarmId = async function (params) {
-//   try {
-//     const { farmId } = params;
-//     if (!farmId || farmId === ":farmId") {
-//       throw new Error("Farm ID is required.");
-//     }
-
-//     const sheepCount = await Livestock.count({
-//       where: {
-//         farm_id: farmId,
-//         type_id: "SHEEP",
-//         gender: "MALE"
-//       }
-//     });
-//     return { count: sheepCount };
-
-//   } catch (error) {
-//     console.error("Error getting number of male sheep:", error.message);
-//     throw error;
-//   }
-// };
-
-exports.getLivestockCountByFarmId = async function (farmId, filters) {
-  try {
-    if (!farmId || farmId === ":farmId") {
-      throw new Error("Farm ID is required.");
-    }
-
-    const count = await Livestock.count({where: filters});
+    
     return { count: count };
 
   } catch (error) {
